d034a9a8061df1793409c105896dbed25f704063
Guillermo Tapia <guillermotd23@gmail.com>
2024-08-14 23:14:38 -0400

    primer ajuste para la ruta login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,29 @@
 import UserModel from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 
-export const Login = (req, res) => {
-  res.send("login");
+export const Login = async (req, res) => {
+  //campos enviados por el front end
+  const { email, username, password } = req.body;
+
+  //validar si existe el usuario ingresado o el correo
+  const userExist = await UserModel.findOne({ username });
+  const userEmail = await UserModel.findOne({ email });
+
+
+  //Se valida que tanto el usuario como el email enviados por el front end no sean ni null ni undefined
+  // if ((userExist !== null && !userExist !== undefined) && (!userEmail !== null && !userEmail !== undefined)) {
+  //   console.log("tan lo do");
+  // }
+
+  //Se valida que al menos el usuario o el correo sean enviados 
+  if ((userExist === null || undefined) || (userEmail === null || undefined)) {
+    console.log("uno de los dos esta vacio")
+  }
+
+  console.log(userExist);
+  console.log(userEmail);
+
+  res.json("login");
 };
 
 export const Signup = async (req, res) => {
@@ -10,19 +31,15 @@ export const Signup = async (req, res) => {
     //campos enviados por el front end
     const { email, username, password } = req.body;
 
-    // console.log(req.body)
-
     //validar si existe el usuario ingresado o el correo
     const userExist = await UserModel.findOne({ username });
     const emailExist = await UserModel.findOne({ email });
-
 
     if (userExist || emailExist)
       return res.status(400).json({ error: "Usuario ya existe" });
 
     //se encripta contraseña
     const hashedPassword = await bcrypt.hash(password, 10);
-
 
     //Nuevo usuario
     const newUser = new UserModel({
@@ -35,10 +52,9 @@ export const Signup = async (req, res) => {
 
     await newUser.save();
 
-    res.send("Usuario registrado")
+    res.send("Usuario registrado");
   } catch (error) {
     console.log(error);
     res.send({ err: error });
   }
-
 };
